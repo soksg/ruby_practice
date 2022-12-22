205f7f48d0c2182b5c9c68a6bd7e78a3f6cd0991
EC2 Default User <ec2-user@ip-172-31-10-166.ap-northeast-1.compute.internal>
2022-12-22 02:58:43 +0000

    [Add]ruby復讐

---
+++ lesson.rb
@@ -0,0 +1,21 @@
+# subjects = ["国語", "算数", "理科", "社会"]
+# puts subjects[2]
+
+# tall = {"taro" => 180, "ziro" => 178, "goro" => 175}
+# puts tall["ziro"]
+
+# def total_price = 101
+#   if total_price > 100
+#     puts "みかんを購入。所持金に余りあり"
+#   elsif total_price == 100
+#     puts "みかんを購入。所持金は０円"
+#   else
+#     puts "みかんを購入できません。"
+# end
+
+dice = 0
+while dice != 6 do
+  dice=rand(1..6)
+  puts dice
+end
+
