9b9cfb1f597b4d37241c83a5a179b681cbbb0341
EC2 Default User <ec2-user@ip-172-31-10-166.ap-northeast-1.compute.internal>
2022-12-30 06:46:35 +0000

    [Add]素数判定を追加

--- lesson.rb
+++ lesson.rb
@@ -19,15 +19,42 @@
 #   puts dice
 # end
 
-(1..30).each do |number|
-  if number % 15 == 0
-    puts "fizz_buzz"
-  elsif number % 3 == 0
-    puts "fizz"
-  elsif number % 5 == 0
-    puts "buzz"
-  else
-    puts number.to_s
-  end
-end
+# (1..30).each do |number|
+#   if number % 15 == 0
+#     puts "fizz_buzz"
+#   elsif number % 3 == 0
+#     puts "fizz"
+#   elsif number % 5 == 0
+#     puts "buzz"
+#   else
+#     puts number.to_s
+#   end
+# end
 
+# class Car
+#   def self.turn(direction)
+#     puts "#{direction}に曲がります"
+#   end
+# end
+
+# Car.turn("右")
+
+# (1..30).each do |number|
+#   if number%15==0
+#     puts "FizzBuzz"
+#   elsif number%3==0
+#     puts "Fizz"
+#   elsif number%5==0
+#     puts "Buzz"
+#   else
+#     puts number.to_s
+#   end
+# end
+
+#素数を表示
+number=*1..100
+def is_prime?(n)
+    return false if n==1
+    (2..n-1).map{ |i| return false if n%i==0 }
+end
+number.map{ |n| puts n if is_prime?(n)}
